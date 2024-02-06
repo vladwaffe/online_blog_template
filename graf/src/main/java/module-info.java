1819fb07a60a34637b2057ad3305fdfa28dc4929
vladwaffe <113841179+vladwaffe@users.noreply.github.com>
2024-02-06 22:40:46 +0300

    Delete graf directory

--- graf/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.main.graf {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.main.graf to javafx.fxml;
-    exports com.main.main;
-    opens com.main.main to javafx.fxml;
-}
