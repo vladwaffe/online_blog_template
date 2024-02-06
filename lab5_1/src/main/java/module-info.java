d31589da933388afeefc8311e646500d5284cece
vladwaffe <113841179+vladwaffe@users.noreply.github.com>
2024-02-06 22:40:31 +0300

    Delete lab5_1/src/main directory

--- lab5_1/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.lab5_1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.hibernate.orm.core;
-    requires jakarta.persistence;
-    opens com.lab5_1 to javafx.fxml;
-    exports com.lab5_1;
-    requires java.naming;
-}
